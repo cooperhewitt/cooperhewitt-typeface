16f51b00ceba756a8002ddca0671a9d000b64e71
Adrien Tétar <adri-from-59@hotmail.fr>
2015-07-21 01:17:13 +0200

    Update build.py

--- build.py
+++ build.py
@@ -1,18 +1,17 @@
 from __future__ import print_function
 from ufo2fdk import OTFCompiler
-from ufo2fdk.makeotfParts import MakeOTFPartsCompiler
 from robofab.world import OpenFont
 import sys
 
 family = "CooperHewitt"
 weights = ["Thin", "Light", "Book", "Medium", "Semibold", "Bold", "Heavy"]
-for w in weights[0:]:
+for w in weights[:]:
     weights.append(w + "Italic")
 
 for w in weights:
     font = OpenFont("files/{}-{}.ufo".format(family, w))
     compiler = OTFCompiler(savePartsNextToUFO = len(sys.argv)>1 and sys.argv[1] == "debug")
-    reports = compiler.compile(font, family + "-" + w + ".otf", checkOutlines=False, autohint=True, releaseMode=True)
+    reports = compiler.compile(font, "{}-{}.otf".format(family, w), checkOutlines=False, autohint=True, releaseMode=True)
 
     print(reports["autohint"])
     print(reports["makeotf"])
